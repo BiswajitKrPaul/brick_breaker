8a331d679b31ababa20412e12f5955c20b785a2f
Biswajit Paul <biswajitkmrpl46@gmail.com>
2021-07-08 21:08:42 +0530

    Movement completed

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -12,126 +12,51 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
-  final breaker = [
-    [29, 7],
-    [29, 8],
-    [29, 9],
-    [29, 10],
-  ];
+  List breaker = [...kBreaker];
 
-  List ball = [
-    [15, 8]
-  ];
-
-  List brick = [
-    [0, 0],
-    [0, 1],
-    [0, 2],
-    [0, 3],
-    [0, 4],
-    [0, 5],
-    [0, 6],
-    [0, 7],
-    [0, 8],
-    [0, 9],
-    [0, 10],
-    [0, 11],
-    [0, 12],
-    [0, 13],
-    [0, 14],
-    [0, 15],
-    [0, 16],
-    [0, 17],
-    [1, 0],
-    [1, 1],
-    [1, 2],
-    [1, 3],
-    [1, 4],
-    [1, 5],
-    [1, 6],
-    [1, 7],
-    [1, 8],
-    [1, 9],
-    [1, 10],
-    [1, 11],
-    [1, 12],
-    [1, 13],
-    [1, 14],
-    [1, 15],
-    [1, 16],
-    [1, 17],
-    [2, 0],
-    [2, 1],
-    [2, 2],
-    [2, 3],
-    [2, 4],
-    [2, 5],
-    [2, 6],
-    [2, 7],
-    [2, 8],
-    [2, 9],
-    [2, 10],
-    [2, 11],
-    [2, 12],
-    [2, 13],
-    [2, 14],
-    [2, 15],
-    [2, 16],
-    [2, 17],
-    [3, 0],
-    [3, 1],
-    [3, 2],
-    [3, 3],
-    [3, 4],
-    [3, 5],
-    [3, 6],
-    [3, 7],
-    [3, 8],
-    [3, 9],
-    [3, 10],
-    [3, 11],
-    [3, 12],
-    [3, 13],
-    [3, 14],
-    [3, 15],
-    [3, 16],
-    [3, 17],
-    [4, 0],
-    [4, 1],
-    [4, 2],
-    [4, 3],
-    [4, 4],
-    [4, 5],
-    [4, 6],
-    [4, 7],
-    [4, 8],
-    [4, 9],
-    [4, 10],
-    [4, 11],
-    [4, 12],
-    [4, 13],
-    [4, 14],
-    [4, 15],
-    [4, 16],
-    [4, 17],
-  ];
+  List ball = [...kBall];
+  List brick = [...kBrick];
   Direction direction;
   BallDirection ballDirection = BallDirection.Down;
   bool isGameStarted = false;
+  bool isGameOver = false;
   Timer timer;
 
   void startGame() {
     isGameStarted = true;
-    timer = Timer.periodic(Duration(milliseconds: 300), (timer) {
+    isGameOver = false;
+    timer = Timer.periodic(Duration(milliseconds: 100), (timer) {
       setState(() {
-        moveBall();
+        if (isGameOver) {
+          timer.cancel();
+          restart();
+        } else {
+          moveBall();
+        }
       });
+    });
+  }
+
+  void restart() {
+    setState(() {
+      isGameStarted = false;
+      ball = [...kBall];
+      brick = [...kBrick];
+      breaker = [...kBreaker];
+      ballDirection = BallDirection.Down;
     });
   }
 
   bool hasBallHitBrick() {
     for (int i = 0; i < brick.length; i++) {
       if (brick[i].last == ball.first[1] && brick[i].first == ball.first[0]) {
+        if (ballDirection == BallDirection.Up) {
+          ballDirection = BallDirection.Down;
+        } else if (ballDirection == BallDirection.LeftDiagonalUp) {
+          ballDirection = BallDirection.RightDiagonalDown;
+        } else if (ballDirection == BallDirection.RightDiagonalUp) {
+          ballDirection = BallDirection.leftDiagonalDown;
+        }
         brick.removeAt(i);
         return true;
       }
@@ -144,49 +69,128 @@ class _MainScreenState extends State<MainScreen> {
        2. Check if the ball is hitting the brick
        3. Check if the ball is Breaker
     */
-
-    if (ballDirection == BallDirection.Down) {
-      if (ball.first[0] == kTotalNoColumn - 1) {
-        ballDirection = BallDirection.Up;
-      } else if (ball.first[0] == kTotalNoColumn - 2) {
-        if (breaker[1].last == ball.first[1] ||
-            breaker[2].last == ball.first[1]) {
+    if (ball.first[0] != kTotalNoColumn - 1) {
+      if (ballDirection == BallDirection.Down) {
+        if (ball.first[0] == kTotalNoColumn - 2) {
+          if (breaker[1].last == ball.first[1] ||
+              breaker[2].last == ball.first[1]) {
+            ballDirection = BallDirection.Up;
+          } else if (breaker[0].last == ball.first[1]) {
+            ballDirection = BallDirection.LeftDiagonalUp;
+          } else if (breaker[3].last == ball.first[1]) {
+            ballDirection = BallDirection.RightDiagonalUp;
+          } else {
+            ball.insert(0, [ball.first[0] + 1, ball.first[1]]);
+            ball.removeLast();
+            ballDirection = BallDirection.Down;
+          }
+        } else {
+          ballDirection = BallDirection.Down;
+          ball.insert(0, [ball.first[0] + 1, ball.first[1]]);
+        }
+      }
+      if (ballDirection == BallDirection.Up) {
+        if (hasBallHitBrick()) {
+          ball.insert(0, [ball.first[0] + 1, ball.first[1]]);
+          ball.removeLast();
+        } else if (ball.first[0] <= 0) {
+          ball.insert(0, [ball.first[0] + 1, ball.first[1]]);
+          ball.removeLast();
+          ballDirection = BallDirection.Down;
+        } else {
+          ball.insert(0, [ball.first[0] - 1, ball.first[1]]);
           ballDirection = BallDirection.Up;
-        } else if (breaker[0].last == ball.first[1]) {
-          ballDirection = BallDirection.LeftDiagonalUp;
-        } else if (breaker[3].last == ball.first[1]) {
+        }
+      }
+      if (ballDirection == BallDirection.LeftDiagonalUp) {
+        if (hasBallHitBrick()) {
+          // right diagonal down
+          ball.insert(0, [ball.last[0] + 1, ball.last[1] + 1]);
+          ball.removeLast();
+        } else if (ball.first[0] <= 0) {
+          ball.insert(0, [ball.last[0] + 1, ball.last[1] + 1]);
+          ballDirection = BallDirection.RightDiagonalDown;
+          ball.removeLast();
+        } else if (ball.first[1] <= 0) {
+          ball.insert(0, [ball.last[0] - 1, ball.last[1] + 1]);
+          ball.removeLast();
           ballDirection = BallDirection.RightDiagonalUp;
         } else {
-          ball.insert(0, [ball.first[0] + 1, ball.first[1]]);
-          ballDirection = BallDirection.Down;
+          ball.insert(0, [ball.first[0] - 1, ball.first[1] - 1]);
+          ballDirection = BallDirection.LeftDiagonalUp;
         }
-      } else {
-        ballDirection = BallDirection.Down;
-        ball.insert(0, [ball.first[0] + 1, ball.first[1]]);
       }
-    }
-    if (ballDirection == BallDirection.Up) {
-      if (hasBallHitBrick()) {
-        ball.insert(0, [ball.first[0] + 1, ball.first[1]]);
-        ballDirection = BallDirection.Down;
-      } else if (ball.first[0] <= 0) {
-        ball.insert(0, [ball.first[0] + 1, ball.first[1]]);
-        ballDirection = BallDirection.Down;
-      } else {
-        ball.insert(0, [ball.first[0] - 1, ball.first[1]]);
-        ballDirection = BallDirection.Up;
+      if (ballDirection == BallDirection.RightDiagonalUp) {
+        if (hasBallHitBrick()) {
+          //left diagonal down
+          ball.insert(0, [ball.first[0] + 1, ball.first[1] - 1]);
+          ball.removeLast();
+        } else if (ball.first[0] <= 0) {
+          ball.insert(0, [ball.first[0] + 1, ball.first[1] - 1]);
+          ballDirection = BallDirection.leftDiagonalDown;
+          ball.removeLast();
+        } else if (ball.first[1] == kTotalNoRow - 1) {
+          ball.insert(0, [ball.first[0] - 1, ball.first[1] - 1]);
+          ball.removeLast();
+          ballDirection = BallDirection.LeftDiagonalUp;
+        } else {
+          ball.insert(0, [ball.last[0] - 1, ball.last[1] + 1]);
+          ballDirection = BallDirection.RightDiagonalUp;
+        }
       }
+      if (ballDirection == BallDirection.RightDiagonalDown) {
+        if (ball.first[1] == kTotalNoRow - 1) {
+          ball.insert(0, [ball.first[0] + 1, ball.first[1] - 1]);
+          ball.removeLast();
+          ballDirection = BallDirection.leftDiagonalDown;
+        } else if (ball.first[0] == kTotalNoColumn - 2) {
+          if (breaker[1].last == ball.first[1] ||
+              breaker[2].last == ball.first[1]) {
+            ballDirection = BallDirection.Up;
+          } else if (breaker[0].last == ball.first[1]) {
+            ballDirection = BallDirection.LeftDiagonalUp;
+          } else if (breaker[3].last == ball.first[1]) {
+            ballDirection = BallDirection.RightDiagonalUp;
+          } else {
+            ball.insert(0, [ball.last[0] + 1, ball.last[1] + 1]);
+            ballDirection = BallDirection.RightDiagonalDown;
+            ball.removeLast();
+          }
+        } else {
+          ball.insert(0, [ball.last[0] + 1, ball.last[1] + 1]);
+          ballDirection = BallDirection.RightDiagonalDown;
+        }
+      }
+      if (ballDirection == BallDirection.leftDiagonalDown) {
+        if (ball.first[1] <= 0) {
+          ballDirection = BallDirection.RightDiagonalDown;
+          ball.insert(0, [ball.first[0] + 1, ball.first[1] + 1]);
+          ball.removeLast();
+        } else if (ball.first[0] == kTotalNoColumn - 2) {
+          if (breaker[1].last == ball.first[1] ||
+              breaker[2].last == ball.first[1]) {
+            ballDirection = BallDirection.Up;
+          } else if (breaker[0].last == ball.first[1]) {
+            ballDirection = BallDirection.LeftDiagonalUp;
+          } else if (breaker[3].last == ball.first[1]) {
+            ballDirection = BallDirection.RightDiagonalUp;
+          } else {
+            ball.insert(0, [ball.first[0] + 1, ball.first[1] - 1]);
+            ballDirection = BallDirection.leftDiagonalDown;
+            ball.removeLast();
+          }
+        } else {
+          ball.insert(0, [ball.first[0] + 1, ball.first[1] - 1]);
+          ballDirection = BallDirection.leftDiagonalDown;
+        }
+      }
+      if (ball.length > 1) {
+        ball.removeLast();
+      }
+    } else {
+      print('Game Over');
+      isGameOver = true;
     }
-    if (ballDirection == BallDirection.LeftDiagonalUp) {
-      ball.insert(0, [ball.first[0] - 1, ball.first[1] - 1]);
-      ballDirection = BallDirection.LeftDiagonalUp;
-    }
-    if (ballDirection == BallDirection.RightDiagonalUp) {
-      ball.insert(0, [ball.last[0] - 1, ball.last[1] + 1]);
-      ballDirection = BallDirection.RightDiagonalUp;
-    }
-
-    ball.removeLast();
   }
 
   void moveBreaker(Direction direction) {
@@ -201,7 +205,6 @@ class _MainScreenState extends State<MainScreen> {
         breaker.removeAt(0);
       }
     }
-
     setState(() {
       this.direction = direction;
     });
@@ -217,81 +220,110 @@ class _MainScreenState extends State<MainScreen> {
           'Brick Breaker',
           textAlign: TextAlign.center,
         ),
+        actions: [
+          IconButton(
+            onPressed: restart,
+            icon: Icon(Icons.restart_alt),
+          ),
+        ],
       ),
       body: GestureDetector(
         onTap: isGameStarted ? null : startGame,
-        child: Column(
+        child: Stack(
+          alignment: Alignment.center,
           children: [
-            SizedBox(
-              height: 8,
-            ),
-            Expanded(
-              flex: 4,
-              child: Container(
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.black,
-                    width: 1,
-                  ),
+            Column(
+              children: [
+                SizedBox(
+                  height: 8,
                 ),
-                child: AspectRatio(
-                  aspectRatio: kTotalNoRow / kTotalNoColumn,
+                Expanded(
+                  flex: 4,
                   child: Container(
-                    color: Colors.transparent,
-                    child: GridView.builder(
-                      physics: NeverScrollableScrollPhysics(),
-                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: kTotalNoRow,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.black,
+                        width: 1,
                       ),
-                      itemCount: kTotalNoRow * kTotalNoColumn,
-                      itemBuilder: (BuildContext context, int index) {
-                        int rowY = index % kTotalNoRow;
-                        int rowX = (index / kTotalNoRow).floor();
-                        bool isBreaker = false;
-                        bool isBrick = false;
-                        Color color;
-                        for (var pos in breaker) {
-                          if (pos[0] == rowX && pos[1] == rowY) {
-                            isBreaker = true;
-                            break;
-                          }
-                        }
+                    ),
+                    child: AspectRatio(
+                      aspectRatio: kTotalNoRow / kTotalNoColumn,
+                      child: Container(
+                        color: Colors.transparent,
+                        child: GridView.builder(
+                          physics: NeverScrollableScrollPhysics(),
+                          gridDelegate:
+                              SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: kTotalNoRow,
+                          ),
+                          itemCount: kTotalNoRow * kTotalNoColumn,
+                          itemBuilder: (BuildContext context, int index) {
+                            int rowY = index % kTotalNoRow;
+                            int rowX = (index / kTotalNoRow).floor();
+                            bool isBreaker = false;
+                            bool isBrick = false;
+                            Color color;
+                            for (var pos in breaker) {
+                              if (pos[0] == rowX && pos[1] == rowY) {
+                                isBreaker = true;
+                                break;
+                              }
+                            }
 
-                        for (var pos in brick) {
-                          if (pos[0] == rowX && pos[1] == rowY) {
-                            isBrick = true;
-                            break;
-                          }
-                        }
+                            for (var pos in brick) {
+                              if (pos[0] == rowX && pos[1] == rowY) {
+                                isBrick = true;
+                                break;
+                              }
+                            }
 
-                        if (isBreaker) {
-                          color = Colors.red;
-                        } else if (isBrick) {
-                          color = Colors.deepPurple;
-                        } else if (rowX == ball.first[0] &&
-                            rowY == ball.first[1]) {
-                          color = Colors.green;
-                        } else {
-                          color = Colors.transparent;
-                        }
+                            if (isBreaker) {
+                              color = Colors.red;
+                            } else if (isBrick) {
+                              color = Colors.deepPurple;
+                            } else if (rowX == ball.first[0] &&
+                                rowY == ball.first[1]) {
+                              color = Colors.green;
+                            } else {
+                              color = Colors.transparent;
+                            }
 
-                        return Container(
-                          margin: EdgeInsets.all(1),
-                          color: color,
-                        );
-                      },
+                            return Container(
+                              margin: EdgeInsets.all(1),
+                              color: color,
+                            );
+                          },
+                        ),
+                      ),
                     ),
                   ),
                 ),
-              ),
+                Expanded(
+                  flex: 1,
+                  child: GameController(
+                    direction: direction,
+                    function: moveBreaker,
+                  ),
+                ),
+              ],
             ),
-            Expanded(
-              flex: 1,
-              child: GameController(
-                direction: direction,
-                function: moveBreaker,
-              ),
-            ),
+            isGameStarted
+                ? Container()
+                : Container(
+                    height: 200,
+                    width: 150,
+                    color: Colors.black54,
+                    child: Center(
+                      child: Text(
+                        'Tap to play',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
           ],
         ),
       ),
